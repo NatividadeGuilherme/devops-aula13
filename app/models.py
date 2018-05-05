4c8235a51d916a810ef7d922af995d17547aec38
Guilherme Natividade <36283471+NatividadeGuilherme@users.noreply.github.com>
2018-05-04 22:01:26 -0300

    Update models.py

--- app/models.py
+++ app/models.py
@@ -26,3 +26,9 @@ class Candidato(models.Model):
     cpf = models.CharField(max_length=20)
     endereco = models.CharField(max_length=100)
     telefone = models.CharField(max_length=20)
+    
+class LocaisProva(models.Model):
+    nomeEscola = models.CharField(max_length=50)
+    endereco = models.CharField(max_length=100)
+    cidade = models.CharField(max_length=50)
+    estado = models.CharField(max_length=2)
